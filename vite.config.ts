26822bc9fc5d52d175a8df1271a34accfa9f57bb
JJue888 <joshuajue888@gmail.com>
2026-09-17 13:52:50 -0700

    nitro

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,21 @@ import { devtools } from '@tanstack/devtools-vite'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
+import { nitro } from 'nitro/vite'
+
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const isNetlify = process.env.NETLIFY === 'true'
-const isVercel = process.env.VERCEL === '1'
 
 const config = defineConfig(async () => {
-  const plugins = [devtools(), tailwindcss(), tanstackStart(), viteReact()]
+  const plugins = [devtools(), tailwindcss(), tanstackStart(), viteReact(), nitro()]
 
   if (isNetlify) {
     const { default: netlify } = await import('@netlify/vite-plugin-tanstack-start')
     plugins.unshift(netlify())
   }
 
-  if (isVercel) {
-    const { default: vercel } = await import('@tanstack/vite-plugin-tanstack-start-vercel')
-    plugins.unshift(vercel())
-  }
 
   return {
     resolve: { tsconfigPaths: true },
